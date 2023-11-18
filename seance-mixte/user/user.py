2e3a48935e84c7688a48b3bf0c657bf4935a45f2
Manne Emile Kitsoukou <emilemannekitsoukou@gmail.com>
2023-11-18 21:53:21 +0100

    fix: update the open api documentation

--- seance-mixte/user/user.py
+++ seance-mixte/user/user.py
@@ -3,24 +3,28 @@ import datetime
 import json
 
 import requests
-from flask import Flask, request, jsonify, make_response
-
-import grpc
-from booking import booking_pb2
-
-
-
+from flask import Flask, request, jsonify, make_response, Response
 
 app = Flask(__name__)
 
-PORT = 3203
+PORT = 3004
 HOST = '0.0.0.0'
 
-with open('{}/databases/users.json'.format("."), "r") as jsf:
+with open('{}/data/users.json'.format("."), "r") as jsf:
     users = json.load(jsf)["users"]
 
 
-    
+##################### Utilities #####################
+def update_user_last_active(user_id):
+    """
+    Update the last active date of a user
+    :param user_id: id of the user
+    :return: None
+    """
+    index = users.index(user_id)
+
+    if index != -1:
+        users[index]["last_active"] = str(datetime.datetime.now().timestamp())
 
 
 @app.route("/", methods=['GET'])
@@ -29,7 +33,11 @@ def home():
 
 
 @app.route("/user/", methods=['POST'])
-def add_user():
+def add_user() -> Response:
+    """
+    Add a user to the database
+    :return: Response object with a message and the id of the user
+    """
     # Récupération des données de la requête
     data = request.get_json()
     if data is None:
@@ -38,33 +46,37 @@ def add_user():
     # Vérification de la validité des données
     errors = []
 
-    if "firstname" not in data:
-        errors.append({"firstname": "Missing field"})
+    # String vide ou non présent
+    if "first_name" not in data or data["first_name"].strip() == "":
+        errors.append({"first_name": "Missing field"})
 
-    if "lastname" not in data:
-        errors.append({"lastname": "Missing field"})
+    if "last_name" not in data or data["last_name"].strip() == "":
+        errors.append({"last_name": "Missing field"})
 
     if errors:
-        return make_response(jsonify({"errors": errors}), 400)
+        return make_response(jsonify({"errors": errors, "message": "Invalid data"}), 400)
 
     # id de l'utilisateur
-    user_id = (data["firstname"] + "_" + data["lastname"]).lower()
+    user_id = (data["first_name"] + "_" + data["last_name"]).lower()
 
     # Verification si l'id est dans la base de donnees
     for user in users:
         if user["id"] == user_id:
             return make_response(jsonify({"error": "User already exists"}), 400)
 
-    # La derniere connexion est au moment de l'ajout en secondes
-    last_active = str(datetime.datetime.now().timestamp())
-
-    users.append({
+    user = {
         "id": user_id,
-        "name": data["firstname"] + " " + data["lastname"],
-        "last_active": last_active,
-    })
+        "name": data["first_name"] + " " + data["last_name"],
+        "last_active": str(datetime.datetime.now().timestamp())
+    }
 
-    return make_response(jsonify({"message": "User added", "id": user_id}), 201)
+    users.append(user)
+
+    # Mis à jour de la base de données
+    with open('{}/data/users.json'.format("."), "w") as jsf:
+        json.dump({"users": users}, jsf)
+
+    return make_response(jsonify({"message": "User added", "data": user}), 201)
 
 
 @app.route("/user/list", methods=['GET'])
@@ -120,77 +132,6 @@ def delete_user(user_id):
 
     return make_response(jsonify({"error": "User not found"}), 404)
 
-
-@app.route("/user/<user_id>/bookings", methods=['GET'])
-def get_user_bookings(user_id):
-    # Vérification si l'id est dans la base de donnees
-
-    check_user = requests.get(f"http://{HOST}:{PORT}/user/{user_id}")
-
-    if check_user.ok:
-        return requests.get(f"http://{HOST}:3201/bookings/{user_id}")
-
-    return make_response(jsonify({"error": "Impossible to retrieve this user's bookings"}), 404)
-
-
-@app.route("/user/<user_id>/bookings", methods=['POST'])
-def add_user_booking(user_id):
-    # Vérification si l'id est dans la base de donnees
-
-    check_user = requests.get(f"http://{HOST}:{PORT}/user/{user_id}")
-
-    if check_user.ok:
-        # On interroge le service de réservation
-
-        return requests.post(f"http://{HOST}:3201/bookings/{user_id}", json=request.get_json())
-
-    return make_response(jsonify({"error": "Impossible to add this user's bookings"}), 404)
-
-
-@app.route("/user/<user_id>/movies", methods=['GET'])
-def get_user_movies(user_id):
-    # Vérification si l'id est dans la base de donnees
-
-    check_user = requests.get(f"http://{HOST}:{PORT}/user/{user_id}")
-
-    if not check_user.ok:
-        return make_response(jsonify({"error": "User not found"}), 404)
-
-    # On interroge le service de réservation pour récupérer les réservations de l'utilisateur
-
-    bookings = requests.get(f"http://{HOST}:3201/bookings/{user_id}")
-
-    if not bookings.ok:
-        return make_response(jsonify({"error": "Impossible to retrieve this user's bookings"}), 404)
-
-    # On interroge le service de films pour récupérer les films
-
-    # Requete graphQl pour récuperer la liste des films via la fonction all_movies
-    graphQl_query = """
-    {
-        allMovies {
-            id
-        }
-    }
-    """
-
-    movies = requests.post(f"http://{HOST}:3200/graphql", json={"query": graphQl_query})
-
-    if not movies.ok:
-        return make_response(jsonify({"error": "Impossible to retrieve movies"}), 404)
-
-    # On récupère les films réservés par l'utilisateur
-
-    user_movies = []
-
-    for booking in bookings.json():
-        for movie in movies.json():
-            if movie["id"] in booking["movies"]:
-                user_movies.append(movie)
-
-    return make_response(jsonify(user_movies), 200)
-
-
 if __name__ == "__main__":
-    print("Server running in port %s" % (PORT))
+    print("Server running in port %s" % PORT)
     app.run(host=HOST, port=PORT)
